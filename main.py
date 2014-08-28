24ebbc36b4493b26b10ed656e4a53deccdcf5d6a
Ben Eggers <beggerss@cs.washington.edu>
2014-08-27 19:38:36 -0700

    main program

---
+++ main.py
@@ -0,0 +1,13 @@
+# Author: Ben Eggers <ben.eggers36@gmail.com>
+#
+# This script reads dictionary.txt and checks to see if any two words
+# are the result of the same sequence of key-presses, one on a qwerty
+# keyboard and one on a dvorak keyboard.
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
