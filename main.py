dbe38ee8de971b11e8b279c93b5a7fcbff3b0c7c
Ben Eggers <beggerss@cs.washington.edu>
2014-08-27 20:27:00 -0700

    made things prettier

--- main.py
+++ main.py
@@ -4,44 +4,6 @@
 # are the result of the same sequence of key-presses, one on a qwerty
 # keyboard and one on a dvorak keyboard.
 
-# {qwerty -> dvorak} character. On qwerty, q = 1, w = 2, etc. We're also going
-# to add ', ,, ;, and . because they're used as letters on the dvorak keyboard.
-
-# I use a dvorak keyboard with a qwerty layout painted on the keys and
-# jesus was this hard.
-CHAR_MAP = {
-        'q': '\'',
-        'w': ',',
-        'e': '.',
-        'r': 'p',
-        't': 'y',
-        'y': 'f',
-        'u': 'g',
-        'i': 'c',
-        'o': 'r',
-        'p': 'l',
-        'a': 'a',
-        's': 'o',
-        'd': 'e',
-        'f': 'u',
-        'g': 'i',
-        'h': 'd',
-        'j': 'h',
-        'k': 't',
-        'l': 'n',
-        ';': 's',
-        'z': ';',
-        'x': 'q',
-        'c': 'j',
-        'v': 'k',
-        'b': 'x',
-        'n': 'b',
-        'm': 'm',
-        ',': 'w',
-        '.': 'v',
-        '/': 'z'
-    }
-
 
 def main():
     dict = open("dictionary.txt", "r")
@@ -85,6 +47,42 @@ def report_matches(matches):
     print("QWERTY\t\tDVORAK\n")
     for match in matches:
         print("%s\t\t%s" % match)
+
+
+# {qwerty -> dvorak} character. On qwerty, q = 1, w = 2, etc. We're also going
+# to add ', ,, ;, and . because they're used as letters on the dvorak keyboard.
+CHAR_MAP = {
+        'q': '\'',
+        'w': ',',
+        'e': '.',
+        'r': 'p',
+        't': 'y',
+        'y': 'f',
+        'u': 'g',
+        'i': 'c',
+        'o': 'r',
+        'p': 'l',
+        'a': 'a',
+        's': 'o',
+        'd': 'e',
+        'f': 'u',
+        'g': 'i',
+        'h': 'd',
+        'j': 'h',
+        'k': 't',
+        'l': 'n',
+        ';': 's',
+        'z': ';',
+        'x': 'q',
+        'c': 'j',
+        'v': 'k',
+        'b': 'x',
+        'n': 'b',
+        'm': 'm',
+        ',': 'w',
+        '.': 'v',
+        '/': 'z'
+    }
 
 
 if __name__ == "__main__":
